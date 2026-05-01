a72c9473e5583526b19a4b78cc95b1830863ced7
Abhishek Sharma <sharmacktdp@gmail.com>
2026-05-01 11:49:19 +0530

    Initialize passport for Google login

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import helmet from "helmet";
 import path from "path";
 import { fileURLToPath } from "url";
 
+import passport from "./config/passport.js";
 import authRoutes from "./routes/authRoutes.js";
 import tournamentRoutes from "./routes/tournamentRoutes.js";
 import entryRoutes from "./routes/entryRoutes.js";
@@ -72,6 +73,7 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
+app.use(passport.initialize());
 
 console.log("STATIC UPLOADS PATH:", path.join(__dirname, "uploads"));
 
